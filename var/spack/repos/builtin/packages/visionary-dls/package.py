32f21cda5cae3f627952f7be28f3fdcd67c43c93
Arne Emmel <arne.emmel@kip.uni-heidelberg.de>
2019-10-09 14:01:59 +0200

    visionary-dls: Add py-sacred
    Change-Id: I4b9b35e7c8da6e6e3a4182c935b86065cf6d2c87

--- var/spack/repos/builtin/packages/visionary-dls/package.py
+++ var/spack/repos/builtin/packages/visionary-dls/package.py
@@ -34,6 +34,7 @@ class VisionaryDls(Package):
     depends_on('py-pynn')
     depends_on('py-python-socketio')
     depends_on('py-pytorch-ignite')
+    depends_on('py-sacred')
     depends_on('py-scikit-learn')
     depends_on('py-seaborn')
     depends_on('py-soundfile')
