60bcd6c9a3d0015e157a669e6fc2e706ed374bc8
Andreas Baumbach <andreas.baumbach@kip.uni-heidelberg.de>
2020-01-24 11:00:27 +0100

    correct py-tables python dependency only @3.6: needs py3:
    Change-Id: Ic9de774238329c97b82f06538ad7fae8f2b9fd9c

--- var/spack/repos/builtin/packages/py-tables/package.py
+++ var/spack/repos/builtin/packages/py-tables/package.py
@@ -32,7 +32,7 @@ class PyTables(PythonPackage):
     variant('lzo', default=False, description='Support for lzo compression')
 
     # requirements.txt
-    depends_on('python@3.5:', when='@3.4:', type=('build', 'run'))
+    depends_on('python@3.5:', when='@3.6:', type=('build', 'run'))
     depends_on('python@2.6:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-cython@0.21:', type='build')
