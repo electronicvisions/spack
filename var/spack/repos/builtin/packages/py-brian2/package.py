91fe1d41fe458da354b02ee91664f55e5083ef25
Eric Müller <mueller@kip.uni-heidelberg.de>
2020-02-20 13:43:45 +0100

    Add new versions of py-brian2
    Change-Id: I4a9e3f944d3dd6896d0c826661159581824f2ab7

--- var/spack/repos/builtin/packages/py-brian2/package.py
+++ var/spack/repos/builtin/packages/py-brian2/package.py
@@ -12,6 +12,9 @@ class PyBrian2(PythonPackage):
     homepage = "http://www.briansimulator.org"
     url      = "https://pypi.io/packages/source/B/Brian2/Brian2-2.0.1.tar.gz"
 
+    version('2.3', sha256='7c725209e0e907876e420477b5c8fef31326820f3e38a46a54dff55d669478b7')
+    version('2.2.2.1', sha256='02075f66d42fd243fc5e28e1add8862709ae9fdabaffb69858e6d7f684a91525')
+    version('2.1.3.1', sha256='6d8e8a7abdcef6366880a43240ab15daaa1ebfda0c14f84e99d74dba49ec1fc8')
     version('2.0.1', sha256='195d8ced0d20e9069917776948f92aa70b7457bbc6b5222b8199654402ee1153')
     version('2.0rc3', sha256='05f347f5fa6b25d1ce5ec152a2407bbce033599eb6664f32f5331946eb3c7d66')
 
