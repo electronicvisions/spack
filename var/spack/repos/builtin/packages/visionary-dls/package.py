2fa0f53de93e5d4185ff204dc3c6857bb56a41b9
Arne Emmel <arne.emmel@kip.uni-heidelberg.de>
2020-03-19 10:55:09 +0100

    visionary-dls: Add py-pytorch-ignite
    Change-Id: I6476e90f2fe4c8f650fd745ada09be8fbf9a0089

--- var/spack/repos/builtin/packages/visionary-dls/package.py
+++ var/spack/repos/builtin/packages/visionary-dls/package.py
@@ -32,6 +32,7 @@ class VisionaryDls(Package):
     depends_on('py-pandas')
     depends_on('py-pynn')
     depends_on('py-python-socketio')
+    depends_on('py-pytorch-ignite')
     depends_on('py-scikit-learn')
     depends_on('py-seaborn')
     depends_on('py-sqlalchemy')
