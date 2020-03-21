e757a32f267b97f2b485aa99d068e457b0d477fc
Arne Emmel <arne.emmel@kip.uni-heidelberg.de>
2020-03-21 18:05:20 +0100

    Update py-coverage
    Change-Id: I9b09d7403d28dc90d938355da0a5a994943d5443

--- var/spack/repos/builtin/packages/py-coverage/package.py
+++ var/spack/repos/builtin/packages/py-coverage/package.py
@@ -12,9 +12,11 @@ class PyCoverage(PythonPackage):
     homepage = "http://nedbatchelder.com/code/coverage/"
     url      = "https://pypi.io/packages/source/c/coverage/coverage-4.5.4.tar.gz"
 
+    version('5.0.4', sha256='1b60a95fc995649464e0cd48cecc8288bac5f4198f21d04b8229dc4097d76823')
     version('4.5.4', sha256='e07d9f1a23e9e93ab5c62902833bf3e4b1f65502927379148b6622686223125c')
     version('4.3.4', sha256='eaaefe0f6aa33de5a65f48dd0040d7fe08cac9ac6c35a56d0a7db109c3e733df')
     version('4.0a6', sha256='85c7f3efceb3724ab066a3fcccc05b9b89afcaefa5b669a7e2222d31eac4728d')
 
-    depends_on('python@2.6:2.8,3.3:', type=('build', 'run'))
+    depends_on('python@2.6:2.8,3.3:', type=('build', 'run'), when="@:4.999")
+    depends_on('python@2.7:2.8,3.5:', type=('build', 'run'), when="@5.0.0:")
     depends_on('py-setuptools', type='build')
