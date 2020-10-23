56aa877972f675b6b9bad1062206dc2b16fa44aa
Andreas Baumbach <andreas.baumbach@kip.uni-heidelberg.de>
2020-10-23 08:27:10 +0200

    update source of icu4c [do not merge]
    Change-Id: I741ef3d6cde6bb9a77c01b5379b608ac624aecc6

--- var/spack/repos/builtin/packages/icu4c/package.py
+++ var/spack/repos/builtin/packages/icu4c/package.py
@@ -16,7 +16,7 @@ class Icu4c(AutotoolsPackage):
     list_url = "http://download.icu-project.org/files/icu4c"
     list_depth = 2
 
-    version('64.1', sha256='92f1b7b9d51b396679c17f35a2112423361b8da3c1b9de00aa94fd768ae296e6')
+    version('64.1', sha256='dc75081a6af67904ed52487cad8581c996aad53f32d0cd74c1fc2bf6d3fa44db', url='https://github.com/unicode-org/icu/archive/release-64-1.tar.gz')
     version('60.1', sha256='f8f5a6c8fbf32c015a467972bdb1477dc5f5d5dfea908b6ed218715eeb5ee225')
     version('58.2', sha256='2b0a4410153a9b20de0e20c7d8b66049a72aef244b53683d0d7521371683da0c')
     version('57.1', sha256='ff8c67cb65949b1e7808f2359f2b80f722697048e90e7cfc382ec1fe229e9581')
@@ -29,7 +29,7 @@ class Icu4c(AutotoolsPackage):
 
     depends_on('python', type='build', when='@64.1:')
 
-    configure_directory = 'source'
+    configure_directory = 'icu4c/source'
 
     def url_for_version(self, version):
         url = "http://download.icu-project.org/files/icu4c/{0}/icu4c-{1}-src.tgz"
