c379c65f7275b4b4721fc4a442abd73f2858d059
Eric Müller <mueller@kip.uni-heidelberg.de>
2021-02-18 19:17:15 +0100

    Add py-sphinxcontrib-jupyter
    Change-Id: Id8eeb3cf40ebd9135f0ffb4a0ef0fa87c59b1412

---
+++ var/spack/repos/builtin/packages/py-sphinxcontrib-jupyter/package.py
@@ -0,0 +1,26 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+from spack import *
+
+
+class PySphinxcontribJupyter(PythonPackage):
+    """A Sphinx Extension for Generating Jupyter Notebooks."""
+
+    homepage = "https://sphinxcontrib-jupyter.readthedocs.io"
+    url      = "https://github.com/QuantEcon/sphinxcontrib-jupyter/archive/v0.5.10.tar.gz"
+
+    version('0.5.10', sha256='6eb4bc2b5c62a5dff6972e91c723c09fb84b82f44b68d32f471799722122aa89')
+
+    depends_on('python@2.7:2.8,3.3:', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
+    depends_on('py-sphinx', type=('build', 'run'))
+    depends_on('py-docutils', type=('build', 'run'))
+    depends_on('py-nbformat', type=('build', 'run'))
+    depends_on('py-sphinx', type=('build', 'run'))
+    depends_on('py-dask', type=('build', 'run'))
+    depends_on('py-distributed', type=('build', 'run'))
+    depends_on('py-ipython', type=('build', 'run'))
+    depends_on('py-nbconvert', type=('build', 'run'))
+    depends_on('py-jupyter-client', type=('build', 'run'))
