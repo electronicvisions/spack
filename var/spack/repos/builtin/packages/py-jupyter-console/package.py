a12c4fd69ab3aaffc50fd8e108d76a1a57e801ba
Eric Müller <mueller@kip.uni-heidelberg.de>
2021-02-22 18:40:29 +0100

    py-jupyter-console: New version from spack upstream
    Change-Id: I24efac3be4ae75e25a048266ad207ffe28faa68a

--- var/spack/repos/builtin/packages/py-jupyter-console/package.py
+++ var/spack/repos/builtin/packages/py-jupyter-console/package.py
@@ -12,6 +12,7 @@ class PyJupyterConsole(PythonPackage):
     homepage = "https://github.com/jupyter/jupyter_console"
     url      = "https://github.com/jupyter/jupyter_console/archive/5.0.0.tar.gz"
 
+    version('6.1.0', sha256='838c95c99ce52978e1660e7a30dd933dede158e2f4da1bc5fad1a8fad44570b7')
     version('6.0.0', sha256='5cafb1a3b845dbab143c182f270f66396e1ef6a322c6bee7e6f4b0bf85fcf69e',
             url='https://github.com/jupyter/jupyter_console/archive/v6.0.0.tar.gz')
     version('5.2.0', sha256='371d03aeefcf48967f2f00af4c1709f52d2a688deee33f395c6330e4e8aa171c')
@@ -21,8 +22,12 @@ class PyJupyterConsole(PythonPackage):
     version('4.0.3', sha256='b1867a89b693f247e9089a8f367fa4f27af6eac27930cad2966054adfa7b9aa1')
     version('4.0.2', sha256='116a56763899bbb12c762f865372eb52c08619ef070c237c7f1387e192bfd3df')
 
-    depends_on('python@2.7:2.8,3.3:')
+    depends_on('python@2.7:2.8,3.3:', type=('build', 'run'))
+    depends_on('python@3.5:', type=('build', 'run'), when='@6:')
     depends_on('py-jupyter-client', type=('build', 'run'))
+    depends_on('py-ipython@:5.8.0', type=('build', 'run'), when='@:5')
     depends_on('py-ipython', type=('build', 'run'))
     depends_on('py-ipykernel', type=('build', 'run'))
     depends_on('py-pygments', type=('build', 'run'))
+    depends_on('py-prompt-toolkit@1.0.0:1.999', type=('build', 'run'), when='@:5')
+    depends_on('py-prompt-toolkit@2.0.0:2.999,3.0.2:3.0.999', type=('build', 'run'), when='@6:')
