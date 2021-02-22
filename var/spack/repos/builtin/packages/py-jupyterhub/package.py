cf33375c5e79ea8f1697f93422d1cbfb19b0c29d
Eric Müller <mueller@kip.uni-heidelberg.de>
2021-02-22 19:20:40 +0100

    py-jupyterhub: Add version 1.3.0
    Change-Id: I65ba5f2d824c5488bb6837f2054951e5a4f67655

--- var/spack/repos/builtin/packages/py-jupyterhub/package.py
+++ var/spack/repos/builtin/packages/py-jupyterhub/package.py
@@ -12,6 +12,7 @@ class PyJupyterhub(PythonPackage):
     homepage = "https://pypi.org/project/jupyterhub"
     url      = "https://pypi.io/packages/source/j/jupyterhub/jupyterhub-1.0.0.tar.gz"
 
+    version('1.3.0',    sha256='05ff701209c340c792cd103606c448c52ace772ece858380905ddd1a2136ee8e')
     version('1.1.0',    sha256='852a70225a03abd631b36a207f3ffdf69326a0db4cef539825fde39ec1b713d7')
     version('1.0.0',    sha256='33541a515a041b9a518ca057c1c4ab4215a7450fdddc206401713ee8137fa67f')
     version('0.9.4',    sha256='7848bbb299536641a59eb1977ec3c7c95d931bace4a2803d7e9b28b9256714da')
@@ -19,13 +20,16 @@ class PyJupyterhub(PythonPackage):
     depends_on('python@3.5:', type=('build', 'run'))
     depends_on('py-setuptools', type=('build', 'run'))
     depends_on('py-python-dateutil', type=('build', 'run'))
-    depends_on('py-jinja2', type=('build', 'run'))
+    depends_on('py-jinja2@2.11.0:', type=('build', 'run'), when='@1.3:')
+    depends_on('py-jinja2', type=('build', 'run'), when='@:1.2.999')
     depends_on('py-sqlalchemy@1.1:', type=('build', 'run'))
-    depends_on('py-tornado@5.0:', type=('build', 'run'))
+    depends_on('py-tornado@5.1:', type=('build', 'run'), when='@1.3:')
+    depends_on('py-tornado@5.0:', type=('build', 'run'), when='@:1.2.999')
     depends_on('py-traitlets@4.3.2:', type=('build', 'run'))
     depends_on('py-alembic', type=('build', 'run'))
     depends_on('py-mako', type=('build', 'run'))
-    depends_on('py-async-generator@1.8:', type=('build', 'run'))
+    depends_on('py-async-generator@1.9:', type=('build', 'run'), when='@1.3:')
+    depends_on('py-async-generator@1.8:', type=('build', 'run'), when='@:1.2.999')
     depends_on('py-requests', type=('build', 'run'))
     depends_on('py-certipy@0.1.2:', when='@1.0.0:',  type=('build', 'run'))
     depends_on('py-entrypoints', when='@1.0.0:',  type=('build', 'run'))
@@ -33,8 +37,10 @@ class PyJupyterhub(PythonPackage):
     depends_on('py-python-oauth2@1.0:', when='@:9.4', type=('build', 'run'))
     depends_on('py-pamela', type=('build', 'run'))
     depends_on('py-notebook', type=('build', 'run'))
-    depends_on('py-prometheus-client@0.0.21:', type=('build', 'run'))
+    depends_on('py-prometheus-client@0.4.0:', type=('build', 'run'), when='@1.3:')
+    depends_on('py-prometheus-client@0.0.21:', type=('build', 'run'), when='@:1.2.999')
     depends_on('py-jupyter-telemetry@0.1.0:', when='@1.1.0:', type=('build', 'run'))
+    depends_on('py-psutil@5.6.5:', type=('build', 'run'))
     # npm needed at runtime for configurable-http-proxy
     depends_on('npm', when='@1.1.0:', type=('build', 'run'))
 
