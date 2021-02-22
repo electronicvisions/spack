4d2bcb08655f9a867589b657fd47ccde9b674c71
Eric Müller <mueller@kip.uni-heidelberg.de>
2021-02-22 18:33:51 +0100

    py-tornado: Update dependencies from spack upstream
    Change-Id: I9b7abe45609db400e5d1ccad9ad3a3ccac9a8e6a

--- var/spack/repos/builtin/packages/py-tornado/package.py
+++ var/spack/repos/builtin/packages/py-tornado/package.py
@@ -20,8 +20,9 @@ class PyTornado(PythonPackage):
 
     # requirements from setup.py
     depends_on('python@3.5:', when='@6:', type=('build', 'run'))
-    depends_on('py-backports-ssl-match-hostname', when='^python@:2.7.8', type=('build', 'run'))
+    depends_on('python@2.7:2.8,3.4:', type=('build', 'run'))
+    depends_on('py-backports-ssl-match-hostname', when='@:4 ^python@:2.7.8', type=('build', 'run'))
     depends_on('py-singledispatch', when='^python@:3.3', type=('build', 'run'))
-    depends_on('py-certifi', when='^python@:3.3', type=('build', 'run'))
+    depends_on('py-certifi', when='@:4 ^python@:3.3', type=('build', 'run'))
     depends_on('py-backports-abc@0.4:', when='^python@:3.4', type=('build', 'run'))
-    depends_on('py-futures', when='@:5.999.999 ^python@:3.1.999', type=('build', 'run'))
+    depends_on('py-futures', when='@5: ^python@:3.1', type=('build', 'run'))
