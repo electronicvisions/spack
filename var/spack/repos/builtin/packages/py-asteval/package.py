ffceb395deb7d87d7e6369ca1e97b18c69e1f591
Eric Müller <mueller@kip.uni-heidelberg.de>
2021-07-06 13:26:10 +0200

    py-asteval: add version 0.9.16 (py-lmfit refers to it)
    Change-Id: I4af120f303e1bc502a1d6c7a7e790202140f2221

--- var/spack/repos/builtin/packages/py-asteval/package.py
+++ var/spack/repos/builtin/packages/py-asteval/package.py
@@ -13,6 +13,7 @@ class PyAsteval(PythonPackage):
     pypi = "asteval/asteval-0.9.18.tar.gz"
 
     version('0.9.18', sha256='5d64e18b8a72c2c7ae8f9b70d1f80b68bbcaa98c1c0d7047c35489d03209bc86')
+    version('0.9.16', sha256='bc71bb4f9ad3ef8ccdc3b9a711e094fb00d768172cb7ff9a15773673aac9e0cc')
 
     depends_on('python@3.5:',   type=('build', 'run'))
     depends_on('py-setuptools', type='build')
