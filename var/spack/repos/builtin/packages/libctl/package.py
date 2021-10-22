dc43b88cf0d9638eff6226bb739d8b50c5515a4c
Eric Müller <mueller@kip.uni-heidelberg.de>
2021-10-22 18:25:14 +0200

    Update libctl
    Change-Id: I16f91cfab198c66b60407ab5bb2cb3ebeac6bc19

--- var/spack/repos/builtin/packages/libctl/package.py
+++ var/spack/repos/builtin/packages/libctl/package.py
@@ -10,11 +10,16 @@ class Libctl(AutotoolsPackage):
     """libctl is a free Guile-based library implementing flexible
     control files for scientific simulations."""
 
-    homepage = "http://ab-initio.mit.edu/wiki/index.php/Libctl"
-    url      = "http://ab-initio.mit.edu/libctl/libctl-3.2.2.tar.gz"
-    list_url = "http://ab-initio.mit.edu/libctl/old"
+    homepage = "https://github.com/NanoComp/libctl"
+    url      = "https://github.com/NanoComp/libctl/archive/refs/tags/v4.5.0.tar.gz"
 
+    version('4.5.0', sha256='5124551b3c9ed9a0ca599179f5e78318b5a7a1daa06220329a3b2963c848ba03')
     version('3.2.2', sha256='8abd8b58bc60e84e16d25b56f71020e0cb24d75b28bc5db86d50028197c7efbc')
+
+    depends_on('m4', type='build', when='@4.5.0:')
+    depends_on('automake', type='build', when='@4.5.0:')
+    depends_on('autoconf', type='build', when='@4.5.0:')
+    depends_on('libtool', type='build', when='@4.5.0:')
 
     depends_on('guile')
 
@@ -28,3 +33,19 @@ class Libctl(AutotoolsPackage):
             'GUILE_CONFIG={0}'.format(join_path(
                 spec['guile'].prefix.bin, 'guile-config')),
         ]
+
+    # custom ./autogen.sh
+    @run_before('autoreconf')
+    def custom_autogen_sh(self):
+        if not self.spec.satisfies('@4.5.0:'):
+            return
+        autogen_sh = which('./autogen.sh')
+        options = getattr(self, 'configure_flag_args', [])
+        options += ['--prefix={0}'.format(prefix)]
+        options += self.configure_args()
+        autogen_sh(*options)
+
+    @when('@4.5.0:')
+    def configure(self, spec, prefix):
+        # ./autogen.sh does it
+        pass
