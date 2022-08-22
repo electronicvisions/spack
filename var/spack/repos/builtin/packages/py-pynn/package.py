bb0db2853d9e1e893c9c8147687dabccc84f93e6
Christian Mauch <cmauch@kip.uni-heidelberg.de>
2022-08-22 17:01:17 +0200

    Add py-pynn version 0.10.0
    Depends-on:18298
    
    Change-Id: I67836f182322d6cf93e9b9a228d2a554100d8aa9

--- var/spack/repos/builtin/packages/py-pynn/package.py
+++ var/spack/repos/builtin/packages/py-pynn/package.py
@@ -12,9 +12,10 @@ class PyPynn(PythonPackage):
     """
 
     homepage = "http://neuralensemble.org/PyNN/"
-    pypi = "PyNN/PyNN-0.8.3.tar.gz"
-    git      = "https://github.com/NeuralEnsemble/PyNN.git"
+    pypi = "PyNN/PyNN-0.10.0.tar.gz"
+    git = "https://github.com/NeuralEnsemble/PyNN.git"
 
+    version('0.10.0', sha256='04120fe0e03260d664b337e0ac29d985c3fb3684ef35b1add93a66739891c98f')
     version('0.9.6', sha256='d85226800e30bc1692d3f84485c3fa20b921c2ab55f1201d0a3bf23432e16cd2')
     version('0.9.5', sha256='91af2126b639a6a795bfc2709ac49423278c4794b6d0da143908b9afcb415f80')
     version('0.9.1', sha256='bbc60fea3235427191feb2daa0e2fa07eb1c3946104c068ac8a2a0501263b0b1')
@@ -23,16 +24,24 @@ class PyPynn(PythonPackage):
     version('0.8.1', sha256='ce94246284588414d1570c1d5d697805f781384e771816727c830b01ee30fe39')
     version('0.7.5', sha256='15f75f422f3b71c6129ecef23f29d8baeb3ed6502e7a321b8a2596c78ef7e03c')
 
-    depends_on('python@2.6:2.8,3.3:')
-    depends_on('py-jinja2@2.7:',        type=('build', 'run'))
-    depends_on('py-docutils@0.10:',     type=('build', 'run'))
-    depends_on('py-numpy@1.5:',         type=('build', 'run'))
-    depends_on('py-quantities@0.10:',   type=('build', 'run'))
-    depends_on('py-lazyarray@0.2.9:',   type=('build', 'run'))
+    depends_on("python@2.6:2.8,3.3:", type=("build", "run"))
+    depends_on('python@3.7:', type=("build", "run"), when="@0.10.0:")
 
-    depends_on('py-neo@0.3:0.4.1',      type=('build', 'run'), when="@:0.8.3")
-    depends_on('py-neo@0.5.0:',         type=('build', 'run'), when="@0.9.0:")
-    depends_on('py-lazyarray',          type=('build', 'run'), when="@0.9.0:")
+    depends_on("py-jinja2@2.7:", type=("build", "run"))
+    depends_on("py-docutils@0.10:", type=("build", "run"))
+
+    depends_on("py-numpy@1.5:", type=("build", "run"))
+    depends_on('py-numpy@1.16.1:', type=('build', 'run'), when="@0.10.0:")
+
+    depends_on("py-quantities@0.10:", type=("build", "run"))
+    depends_on('py-quantities@0.12.1:', type=('build', 'run'), when="@0.10.0:")
+
+    depends_on("py-lazyarray@0.2.9:", type=("build", "run"))
+    depends_on('py-lazyarray@0.5.0:', type=('build', 'run'), when="@0.10.0:")
+
+    depends_on("py-neo@0.3:0.4.1", type=("build", "run"), when="@:0.8.3")
+    depends_on("py-neo@0.5.0:", type=("build", "run"), when="@0.9.0:")
+    depends_on('py-neo@0.10.0:', type=('build', 'run'), when="@0.10.0:")
 
     depends_on('py-mock@1.0:', type='test')
 
