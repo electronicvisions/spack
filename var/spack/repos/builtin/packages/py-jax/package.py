e5c976be55e9e6c2509cba836219ff099d913574
Eric Müller <mueller@kip.uni-heidelberg.de>
2023-09-05 14:34:01 +0200

    feat(py-jax{,lib}): new versions
    Change-Id: Iaffbe71e2489bc9fbb9083922153aad6b73a8f78

--- var/spack/repos/builtin/packages/py-jax/package.py
+++ var/spack/repos/builtin/packages/py-jax/package.py
@@ -21,6 +21,7 @@ class PyJax(PythonPackage, CudaPackage):
     homepage = "https://github.com/google/jax"
     pypi = "jax/jax-0.2.25.tar.gz"
 
+    version("0.4.13", sha256="03bfe6749dfe647f16f15f6616638adae6c4a7ca7167c75c21961ecfd3a3baaa")
     version("0.4.9", sha256="1ed135cd08f48e4baf10f6eafdb4a4cdae781f9052b5838c09c91a9f4fa75f09")
     version("0.4.3", sha256="d43f08f940aa30eb339965cfb3d6bee2296537b0dc2f0c65ccae3009279529ae")
     version("0.3.25", sha256="18bea69321cb95ea5ea913adfe5e2c1d453cade9d4cfd0dc814ecba9fc0cb6e3")
@@ -30,13 +31,18 @@ class PyJax(PythonPackage, CudaPackage):
 
     depends_on("python@3.7:", type=("build", "run"))
     depends_on("python@3.8:", when="@0.4:", type=("build", "run"))
+    depends_on("python@3.9:", when="@0.4.14:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
+    depends_on("py-numpy@1.22:", when="@0.4.14:", type=("build", "run"))
+    depends_on("py-numpy@1.21:", when="@0.4.9:", type=("build", "run"))
     depends_on("py-numpy@1.20:", when="@0.3:", type=("build", "run"))
     depends_on("py-numpy@1.18:", type=("build", "run"))
     depends_on("py-opt-einsum", type=("build", "run"))
     depends_on("py-scipy@1.2.1:", type=("build", "run"))
     depends_on("py-scipy@1.5:", when="@0.3:", type=("build", "run"))
     depends_on("py-scipy@1.7:", when="@0.4.7:", type=("build", "run"))
+    depends_on("py-ml-dtypes@0.2.0:", when="@0.4.14:", type=("build", "run"))
+    depends_on("py-ml-dtypes@0.1.0:", when="@0.4.9:", type=("build", "run"))
     depends_on("py-ml-dtypes@0.0.3:", when="@0.4.7:", type=("build", "run"))
 
     depends_on("py-jaxlib@0.1.69:", type=("build", "run"), when="~cuda")
@@ -47,6 +53,10 @@ class PyJax(PythonPackage, CudaPackage):
     depends_on("py-jaxlib@0.4.2:+cuda", type=("build", "run"), when="@0.4.3:+cuda")
     depends_on("py-jaxlib@0.4.7:", type=("build", "run"), when="@0.4.9:~cuda")
     depends_on("py-jaxlib@0.4.7:+cuda", type=("build", "run"), when="@0.4.9:+cuda")
+    depends_on("py-jaxlib@0.4.13:+cuda", type=("build", "run"), when="@0.4.13:+cuda")
+    depends_on("py-jaxlib@0.4.13:", type=("build", "run"), when="@0.4.13:~cuda")
+    depends_on("py-jaxlib@0.4.14:+cuda", type=("build", "run"), when="@0.4.14:+cuda")
+    depends_on("py-jaxlib@0.4.14:", type=("build", "run"), when="@0.4.14:~cuda")
 
     for arch in CudaPackage.cuda_arch_values:
         depends_on(
