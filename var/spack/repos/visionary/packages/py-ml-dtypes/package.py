79f93b66a1a4fa524a65e856450caa2a29da0d60
Eric Müller <mueller@kip.uni-heidelberg.de>
2023-11-23 13:26:06 +0100

    fix(py-ml-dtypes): update towards current upstream
    The old installation hack seems to fail nowadays.
    (Weird egg folder below site-packages, etc.)
    
    Change-Id: Ie9e05f5aaadb7621206104626c9ab653d3faae5c

--- var/spack/repos/visionary/packages/py-ml-dtypes/package.py
+++ var/spack/repos/visionary/packages/py-ml-dtypes/package.py
@@ -6,25 +6,36 @@
 from spack import *
 
 
+# VISIONS: based on spack/0.21.0
 class PyMlDtypes(PythonPackage):
-    """ml_dtypes is a stand-alone implementation of several NumPy dtype
-    extensions used in machine learning libraries."""
+    """A stand-alone implementation of several NumPy dtype extensions
+    used in machine learning libraries."""
 
     homepage = "https://github.com/jax-ml/ml_dtypes"
-    pypi = "ml_dtypes/ml_dtypes-0.1.0.tar.gz"
+    pypi = "ml_dtypes/ml_dtypes-0.3.1.tar.gz"
+    git = "https://github.com/jax-ml/ml_dtypes.git"
+    submodules = True
 
-    version("0.1.0", sha256="c1fc0afe63ce99069f9d7e0693a61cfd0aea90241fc3821af9953d0c11f4048a")
+    version("0.3.1", tag="v0.3.1", commit="bbeedd470ecac727c42e97648c0f27bfc312af30")
+    version("0.2.0", tag="v0.2.0", commit="5b9fc9ad978757654843f4a8d899715dbea30e88")
 
-    patch('setup_py.patch')
-
+    # begin VISIONS (modified): bring upstream
     depends_on("python@3.7:")
-    depends_on("py-setuptools", type="build") # ~=67.7.0
+    depends_on("python@3.9:", when="@0.3:", type=("build", "link", "run"))
     depends_on("py-numpy@1.20:", type=("build", "run"))
-    depends_on("py-numpy@1.21.2:", when="^python@3.9:", type=("build", "run"))
-    depends_on("py-numpy@1.23.3:", when="^python@3.10:", type=("build", "run"))
-    depends_on("py-pybind11@2.10.0:", type="build") # ~=2.10.0
+    depends_on("py-numpy@1.21.2:", when="^python@3.10:", type=("build", "run"))
+    depends_on("py-numpy@1.23.3:", when="^python@3.11:", type=("build", "run"))
+    depends_on("py-numpy@1.26.0:", when="^python@3.13:", type=("build", "run"))
+    # end VISIONS
+    # Build dependencies are overconstrained, older versions work just fine
+    depends_on("py-pybind11", type=("build", "link"))
+    depends_on("py-setuptools", type="build")
+
+    # begin VISIONS (added): bring upstream
+    patch('setup_py.patch')
 
     def install(self, spec, prefix):
     # Note: pip install doesn't install the python sources, probably a setuptool version issue
         python('setup.py', 'install', '--prefix=' + prefix,
                *self.install_options(spec, prefix))
+    # end VISIONS
