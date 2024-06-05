8e37a732b184334199aca51ee31b279af7922e1d
Jakob Kaiser <jakob.kaiser@kip.uni-heidelberg.de>
2024-06-05 07:36:50 +0000

    feat: update bear to v0.21.0
    Change-Id: I5d4c760a17452410152ee21d7705ec31545f7ed4

--- var/spack/repos/visionary/packages/bear/package.py
+++ var/spack/repos/visionary/packages/bear/package.py
@@ -6,7 +6,7 @@
 from spack.package import *
 
 
-# VISIONS: based on spack/0.20.0
+# VISIONS: based on spack/0.21.0
 class Bear(CMakePackage):
     """Bear is a tool that generates a compilation database for clang tooling
     from non-cmake build systems."""
@@ -24,10 +24,10 @@ class Bear(CMakePackage):
     version("2.0.4", sha256="33ea117b09068aa2cd59c0f0f7535ad82c5ee473133779f1cc20f6f99793a63e")
 
     depends_on("pkgconfig", when="@3:")
-    depends_on("fmt", when="@3.0.0:")
-    depends_on("grpc", when="@3.0.0:")
+    depends_on("fmt@8", when="@3.0.0:")
+    depends_on("grpc +shared", when="@3.0.0:")
     depends_on("nlohmann-json", when="@3.0.0:")
-    depends_on("spdlog", when="@3.0.0:")
+    depends_on("spdlog +fmt_external", when="@3.0.0:")
     depends_on("cmake@2.8:", type="build")
     depends_on("python", type="build")
     depends_on("googletest", type="test", when="@3:")
