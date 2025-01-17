805e37498d7d3014ca8bf05ed43c0a1220911f20
Ben Kroehs <ben_luca.kroehs@kip.uni-heidelberg.de>
2025-01-17 16:43:14 +0000

    add: new norse version
    Change-Id: I4f8c2ec410fb7f9d5d9aaf885c4068e931b4c34d

--- var/spack/repos/visionary/packages/py-norse/package.py
+++ var/spack/repos/visionary/packages/py-norse/package.py
@@ -12,14 +12,22 @@ class PyNorse(PythonPackage):
     homepage = "https://norse.github.io/norse/"
     pypi = "norse/norse-0.0.7.post1.tar.gz"
 
+    version('1.1.0', sha256='3f70b8579251316761f7a950680136029fcd9d92bbcc53531366d088a8aaf0c8')
     version('0.0.7.post1', sha256='aeea3bd08f47fcfe3b301f1190928dec482938956a1ab8ba568851deed94bda5')
 
-    depends_on('python@3.7.0:', type=('build', 'run'))
+    depends_on('python@3.8.0:', when='@1.1.0', type=('build', 'run'))
+    depends_on('python@3.7.0:', when='@0.0.7.post1', type=('build', 'run'))
     depends_on('py-numpy', type=('build', 'run'))
-    depends_on('py-torch@1.9.0:', type=('build', 'run'))
-    depends_on('py-torchvision@0.10.0:', type=('build', 'run'))
-    depends_on('py-setuptools', type=('build', 'run'))
+    depends_on('py-torch@2.0.0:', when='@1.1.0', type=('build', 'run'))
+    depends_on('py-torch@1.9.0:', when='@0.0.7.post1', type=('build', 'run'))
+    depends_on('py-torchvision@0.15.0:', when='@1.1.0', type=('build', 'run'))
+    depends_on('py-torchvision@0.10.0:', when='@0.0.7.post1', type=('build', 'run'))
+    depends_on('py-setuptools@64:', when='@1.1.0', type=('build'))
+    depends_on('py-setuptools', when='@0.0.7.post1', type=('build', 'run'))
+    depends_on('py-setuptools-scm@8:', when='@1.1.0', type=('build'))
     depends_on('py-pybind11', type=('build', 'link', 'run'))
+    depends_on('py-nir', when='@1.1.0', type=('build', 'run'))
+    depends_on('py-nirtorch', when='@1.1.0', type=('build', 'run'))
 
     def setup_build_environment(self, env):
         include = []
