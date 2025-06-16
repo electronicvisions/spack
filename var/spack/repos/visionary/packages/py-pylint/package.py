aea9417f1f7681afdde82de2371a3e1e1e272ccd
Jakob Kaiser <jakob.kaiser@kip.uni-heidelberg.de>
2025-06-16 14:45:36 +0000

    chore: py-pylint - relax constraint on py-setuptools
    py-pylint does not officially support newer version of setuptools, but it works for us.
    
    Change-Id: I66f777ce56b2144706a9b3a04988c9b26f838194

--- var/spack/repos/visionary/packages/py-pylint/package.py
+++ var/spack/repos/visionary/packages/py-pylint/package.py
@@ -51,7 +51,9 @@ class PyPylint(PythonPackage):
 
     depends_on("py-setuptools-scm", when="@2.8.2", type="build")
     depends_on("py-setuptools@17.1:", type="build")
-    depends_on("py-setuptools@62.6:62", when="@2.15.0:", type="build")
+    # begin VISIONS (modified): we can not concretize with upper limit -> removing it worked for us
+    depends_on("py-setuptools@62.6:", when="@2.15.0:", type="build")
+    # end VISIONS
     depends_on("py-wheel@0.37.1:0.37", when="@2.15.0:", type="build")
     depends_on("py-dill@0.2:", when="@2.13.5:2.15", type=("build", "run"))
     depends_on("py-dill@0.2:", when="@2.16:  ^python@:3.10", type=("build", "run"))
